8feae8af7619b71c3015004e1e59debe0f1ba00d
mhenro <mhenro@gmail.com>
2018-01-08 20:02:17 +0300

    Changed reviews order

--- src/main/java/org/booklink/repositories/ReviewRepository.java
+++ src/main/java/org/booklink/repositories/ReviewRepository.java
@@ -11,10 +11,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Created by mhenr on 04.01.2018.
  */
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
-    @Query("SELECT new org.booklink.models.response.ReviewResponse(r.id, r.book.id, r.book.name, r.book.cover, r.name, r.author.username, r.author.firstName, r.author.lastName, r.score, r.text, r.likes, r.dislikes) FROM Review r WHERE r.book.id = ?1 ORDER BY r.name")
+    @Query("SELECT new org.booklink.models.response.ReviewResponse(r.id, r.book.id, r.book.name, r.book.cover, r.name, r.author.username, r.author.firstName, r.author.lastName, r.score, r.text, r.likes, r.dislikes) FROM Review r WHERE r.book.id = ?1 ORDER BY r.dislikes, r.likes DESC")
     Page<ReviewResponse> getReviewsByBookId(final Long bookId, final Pageable pageable);
 
-    @Query("SELECT new org.booklink.models.response.ReviewResponse(r.id, r.book.id, r.book.name, r.book.cover, r.name, r.author.username, r.author.firstName, r.author.lastName, r.score, r.text, r.likes, r.dislikes) FROM Review r ORDER BY r.name")
+    @Query("SELECT new org.booklink.models.response.ReviewResponse(r.id, r.book.id, r.book.name, r.book.cover, r.name, r.author.username, r.author.firstName, r.author.lastName, r.score, r.text, r.likes, r.dislikes) FROM Review r ORDER BY r.dislikes, r.likes DESC")
     Page<ReviewResponse> getReviews(final Pageable pageable);
 
     @Query("SELECT new org.booklink.models.response.ReviewResponse(r.id, r.book.id, r.book.name, r.book.cover, r.name, r.author.username, r.author.firstName, r.author.lastName, r.score, r.text, r.likes, r.dislikes) FROM Review r WHERE r.id = ?1")
