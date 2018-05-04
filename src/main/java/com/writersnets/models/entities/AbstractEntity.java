a350843f9b884b2ac64de729db2f5b3333e57e55
mhenro <mhenro@gmail.com>
2018-05-05 00:32:09 +0300

    Added missing fields to AbstractEntity

--- src/main/java/com/writersnets/models/entities/AbstractEntity.java
+++ src/main/java/com/writersnets/models/entities/AbstractEntity.java
@@ -1,4 +1,11 @@
 package com.writersnets.models.entities;
 
-public class AbstractEntity {
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.io.Serializable;
+
+public class AbstractEntity implements Serializable {
+    @Version
+    @Column(name = "opt_lock")
+    private Long version;
 }
